340399f1b8ee8dbd6f98ac4e8422f7c205806d96
Dave Townsend <dtownsend@oxymoronical.com>
2006-02-04 15:07:28 +0000

    Updated for VS2005.

---
+++ FTPStore/Properties/AssemblyInfo.cs
@@ -0,0 +1,42 @@
+/*
+ * $HeadURL$
+ * $LastChangedBy$
+ * $Date$
+ * $Revision$
+ */
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FTP Storage API")]
+[assembly: AssemblyDescription("Storage API FTP Plugin")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Blueprint IT Ltd.")]
+[assembly: AssemblyProduct("FTP Storage")]
+[assembly: AssemblyCopyright("Copyright © Blueprint IT Ltd 2006")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]		
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1524fd01-ba5f-4834-9c52-6c0a35e71a48")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
