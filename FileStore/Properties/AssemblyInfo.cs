c797216c72dd7ef797823616170e749ec4f096ac
Dave Townsend <dtownsend@oxymoronical.com>
2006-02-04 15:07:12 +0000

    Updated for VS2005.

---
+++ FileStore/Properties/AssemblyInfo.cs
@@ -0,0 +1,42 @@
+/*
+ * $HeadURL$
+ * $LastChangedBy$
+ * $Date$
+ * $Revision$
+ */
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("File Storage API")]
+[assembly: AssemblyDescription("Storage API File Plugin")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Blueprint IT Ltd.")]
+[assembly: AssemblyProduct("File Storage")]
+[assembly: AssemblyCopyright("Copyright © Blueprint IT Ltd 2006")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]		
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d3b677aa-a30b-44fa-91e5-ec78c1b59a71")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
